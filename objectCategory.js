873d2424202b05f8fd4acce5e5e9810b50c9589a
Leandro Federico Torales <leandrotorales1234@gmail.com>
2022-11-17 01:33:10 -0300

    maquetado y estios de seccion categorias

--- objectCategory.js
+++ objectCategory.js
@@ -1,381 +1,379 @@
 export const category = [
-
   {
-    seccion:"populares",
-    lista:[
+    seccion: "populares",
+    lista: [
       {
-        nombre:"Batido dulce de leche",
-        comentario:"Orgullo argentino",
-        img:'img/dulce-de-leche.jpg',
-        precio:80
+        nombre: "Batido dulce de leche",
+        comentario: "Orgullo argentino",
+        img: "img/dulce-de-leche.jpg",
+        precio: 80,
       },
       {
-        nombre:"Chocolate con helado",
-        comentario:"Clasico",
-        img:'img/chocolate-helado.jpg',
-        precio:250
+        nombre: "Chocolate con helado",
+        comentario: "Clasico",
+        img: "img/chocolate-helado.jpg",
+        precio: 250,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
-    ]
+    ],
   },
 
   {
-    seccion:"pizzas",
-    lista:[
+    seccion: "pizzas",
+    lista: [
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
-      }
-    ]
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+    ],
   },
 
   {
-    seccion:"hamburguesas",
-    lista:[
+    seccion: "hamburguesas",
+    lista: [
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
-      }
-    ]
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+    ],
   },
 
   {
-    seccion:"papas fritas",
-    lista:[
+    seccion: "papas fritas",
+    lista: [
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
-      }
-    ]
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+    ],
   },
 
   {
-    seccion:"wraps",
-    lista:[
+    seccion: "wraps",
+    lista: [
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Vegetariano",
+        comentario: "Muy saludable",
+        img: "./img/wraps-imgs/wrap-vegetariano.jpg",
+        precio: 120,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Mediterraneo",
+        comentario: "Fresco y europeo",
+        img: "./img/wraps-imgs/wrap-mediterraneo.jpg",
+        precio: 150,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Mexicano con guacamole",
+        comentario: "Extremadamente picante",
+        img: "./img/wraps-imgs/wrap-picante.jpeg",
+        precio: 90,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Pollo al curry",
+        comentario: "Muy especiado",
+        img: "./img/wraps-imgs/wrap-polloAlCurry.jpg",
+        precio: 100,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Carne desmechada",
+        comentario: "Se te deshace en la boca",
+        img: "./img/wraps-imgs/wrap-carneDesmechada.jpg",
+        precio: 170,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Huevo y queso",
+        comentario: "Recomendable para la merienda",
+        img: "./img/wraps-imgs/wrap-huevoYQueso.jpg",
+        precio: 110,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Carne en cubos",
+        comentario: "El clásico",
+        img: "./img/wraps-imgs/wrap-carneEnCubos.jpeg",
+        precio: 140,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
-      }
-    ]
+        nombre: "Carne de cerdo y verduras",
+        comentario: "Algo distinto",
+        img: "./img/wraps-imgs/wrap-cerdo.jpg",
+        precio: 130,
+      },
+    ],
   },
 
   {
-    seccion:"mexican food",
-    lista:[
+    seccion: "mexican food",
+    lista: [
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos al pastor",
+        comentario: "Carne marinada y suave",
+        img: "./img/mexicanFood-imgs/mexicanFood-alPastor.jpg",
+        precio: 60,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos de Barbacoa",
+        comentario: "Carne desmechada y sabrosa",
+        img: "./img/mexicanFood-imgs/mexicanFood-barbacoa.jpg",
+        precio: 80,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos de suadero",
+        comentario: "Carne con mas grasa y sabor",
+        img: "./img/mexicanFood-imgs/mexicanFood-suadero.jpg",
+        precio: 75,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos de pescado",
+        comentario: "Pescado rebozado y frito",
+        img: "./img/mexicanFood-imgs/mexicanFood-pescado.jpg",
+        precio: 120,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos de birria",
+        comentario: "Tacos fritos para dipear",
+        img: "./img/mexicanFood-imgs/mexicanFood-birria.jpg",
+        precio: 40,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos de cabeza",
+        comentario: "Carne con particular sabor",
+        img: "./img/mexicanFood-imgs/mexicanFood-cabeza.jpg",
+        precio: 85,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
+        nombre: "Tacos veganos",
+        comentario: "Para todos y todas",
+        img: "./img/mexicanFood-imgs/mexicanFood-veganos.jpg",
+        precio: 95,
       },
       {
-        nombre:"",
-        comentario:"",
-        img:'img/.jpg',
-        precio:80
-      }
-    ]
+        nombre: "Tacos dorados de pollo",
+        comentario: "Tortilla frita y adornados con crema",
+        img: "./img/mexicanFood-imgs/mexicanFood-dorados.jpg",
+        precio: 160,
+      },
+    ],
   },
 
   {
-    seccion:"batidos",
-    lista:[
+    seccion: "batidos",
+    lista: [
       {
-        nombre:"Fresa y platano",
-        comentario:"Combinacion dulce",
-        img:'img/fresa-platano.jpg',
-        precio:300
+        nombre: "Fresa y platano",
+        comentario: "Combinacion dulce",
+        img: "img/fresa-platano.jpg",
+        precio: 300,
       },
       {
-        nombre:"Tres frutos rojos",
-        comentario:"Aguante el rojo",
-        img:'img/frutos-rojos.jpg',
-        precio:80
+        nombre: "Tres frutos rojos",
+        comentario: "Aguante el rojo",
+        img: "img/frutos-rojos.jpg",
+        precio: 80,
       },
       {
-        nombre:"Batido Tropical",
-        comentario:"Suave",
-        img:'img/tropical.jpg',
-        precio:150
+        nombre: "Batido Tropical",
+        comentario: "Suave",
+        img: "img/tropical.jpg",
+        precio: 150,
       },
       {
-        nombre:"Batido de mango",
-        comentario:"Welinton campeón",
-        img:'img/mango.jpg',
-        precio:400
+        nombre: "Batido de mango",
+        comentario: "Welinton campeón",
+        img: "img/mango.jpg",
+        precio: 400,
       },
       {
-        nombre:"Chocolate con helado",
-        comentario:"Clasico",
-        img:'img/chocolate-helado.jpg',
-        precio:250
+        nombre: "Chocolate con helado",
+        comentario: "Clasico",
+        img: "img/chocolate-helado.jpg",
+        precio: 250,
       },
       {
-        nombre:"Batido de nutella",
-        comentario:"Amante del sabor",
-        img:'img/nutella.jpg',
-        precio:100
+        nombre: "Batido de nutella",
+        comentario: "Amante del sabor",
+        img: "img/nutella.jpg",
+        precio: 100,
       },
       {
-        nombre:"Chocolate vegano",
-        comentario:"Sabor unico",
-        img:'img/vegano.jpg',
-        precio:60
+        nombre: "Chocolate vegano",
+        comentario: "Sabor unico",
+        img: "img/vegano.jpg",
+        precio: 60,
       },
       {
-        nombre:"Batido dulce de leche",
-        comentario:"Orgullo argentino",
-        img:'img/dulce-de-leche.jpg',
-        precio:80
-      }
-    ]
+        nombre: "Batido dulce de leche",
+        comentario: "Orgullo argentino",
+        img: "img/dulce-de-leche.jpg",
+        precio: 80,
+      },
+    ],
   },
-
-]
+];
