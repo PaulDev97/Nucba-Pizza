3fb8c760775ae42b72d8f41ffb3b96883bd32f88
Your Name <perezfernando95@gmail.com>
2022-11-26 19:38:42 -0300

    JS completed

--- index.js
+++ index.js
@@ -5,108 +5,161 @@ const ejemplo = [
         seccion: "populares",
         lista: [
             {
-                nombre: "Popular 1",
+                nombre: "Asesina",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/asesina.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Completa",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/completa.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Turín",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/turin.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Verona",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/verona.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Popular 5",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/wrap-carneEnCubos.jpeg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Popular 6",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/wrap-cerdo.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Popular 7",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/mexicanFood-dorados.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Popular 8",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/populares/dulce-de-leche.jpg',
                 precio: 800
-            },
+            }
         ]
     },
     {
-        seccion: "OtraCosa",
+        seccion: "pizzas",
         lista: [
             {
-                nombre: "Popular 1",
+                nombre: "Roma",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
+                img: './assets/pizzas/roma.jpg',
                 precio: 800
             },
             {
-                nombre: "Popular 1",
+                nombre: "Nápoles",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
+                img: './assets/pizzas/napoles.jpg',
+                precio: 900
             },
             {
-                nombre: "Popular 1",
+                nombre: "Venecia",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
+                img: './assets/pizzas/venecia.jpg',
+                precio: 900
             },
             {
-                nombre: "Popular 1",
+                nombre: "Milán",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
+                img: './assets/pizzas/milan.jpg',
+                precio: 950
             },
             {
-                nombre: "Popular 1",
+                nombre: "Florencia",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
+                img: './assets/pizzas/florencia.jpg',
+                precio: 950
             },
             {
-                nombre: "Popular 1",
+                nombre: "Turín",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
+                img: './assets/pizzas/turin.jpg',
+                precio: 1000
             },
             {
-                nombre: "Popular 1",
+                nombre: "Verona",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
+                img: './assets/pizzas/verona.jpg',
+                precio: 1000
             },
             {
-                nombre: "Popular 1",
+                nombre: "Génova",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/pizza.jpg',
-                precio: 800
-            },
+                img: './assets/pizzas/genova.jpg',
+                precio: 1000
+            }
         ]
     },
+    {
+        seccion: "hamburguesas",
+        lista: [
+            {
+                nombre: "Simple",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/simple.jpg',
+                precio: 800
+            },
+            {
+                nombre: "Nera",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/nera.jpg',
+                precio: 900
+            },
+            {
+                nombre: "Pollo",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/pollo.jpg',
+                precio: 900
+            },
+            {
+                nombre: "Paltosa",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/paltosa.jpg',
+                precio: 950
+            },
+            {
+                nombre: "Cebollita",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/cebollita.jpg',
+                precio: 950
+            },
+            {
+                nombre: "Completa",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/completa.jpg',
+                precio: 1000
+            },
+            {
+                nombre: "Pepino",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/pepino.jpg',
+                precio: 1000
+            },
+            {
+                nombre: "Asesina",
+                comentario: "Comentario 1",
+                img: './assets/hamburguesas/asesina.jpg',
+                precio: 1000
+            }
+        ]
+    }
 
 ]
 
