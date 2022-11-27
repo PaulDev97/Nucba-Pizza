dc09dc1cf559055839dfdb6e54b9660ad4b280d9
Your Name <perezfernando95@gmail.com>
2022-11-27 13:20:46 -0300

    JS Updates

--- index.js
+++ index.js
@@ -7,49 +7,49 @@ const ejemplo = [
             {
                 nombre: "Asesina",
                 comentario: "Comentario 1",
-                img: './assets/populares/asesina.jpg',
+                img: './img/populares/asesina.jpg',
                 precio: 800
             },
             {
                 nombre: "Completa",
                 comentario: "Comentario 1",
-                img: './assets/populares/completa.jpg',
+                img: './img/populares/completa.jpg',
                 precio: 800
             },
             {
                 nombre: "Turín",
                 comentario: "Comentario 1",
-                img: './assets/populares/turin.jpg',
+                img: './img/populares/turin.jpg',
                 precio: 800
             },
             {
                 nombre: "Verona",
                 comentario: "Comentario 1",
-                img: './assets/populares/verona.jpg',
+                img: './img/populares/verona.jpg',
                 precio: 800
             },
             {
                 nombre: "Popular 5",
                 comentario: "Comentario 1",
-                img: './assets/populares/wrap-carneEnCubos.jpeg',
+                img: './img/populares/wrap-carneEnCubos.jpeg',
                 precio: 800
             },
             {
                 nombre: "Popular 6",
                 comentario: "Comentario 1",
-                img: './assets/populares/wrap-cerdo.jpg',
+                img: './img/populares/wrap-cerdo.jpg',
                 precio: 800
             },
             {
                 nombre: "Popular 7",
                 comentario: "Comentario 1",
-                img: './assets/populares/mexicanFood-dorados.jpg',
+                img: './img/populares/mexicanFood-dorados.jpg',
                 precio: 800
             },
             {
                 nombre: "Popular 8",
                 comentario: "Comentario 1",
-                img: './assets/populares/dulce-de-leche.jpg',
+                img: './img/populares/dulce-de-leche.jpg',
                 precio: 800
             }
         ]
@@ -60,49 +60,49 @@ const ejemplo = [
             {
                 nombre: "Roma",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/roma.jpg',
+                img: './img/pizzas/roma.jpg',
                 precio: 800
             },
             {
                 nombre: "Nápoles",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/napoles.jpg',
+                img: './img/pizzas/napoles.jpg',
                 precio: 900
             },
             {
                 nombre: "Venecia",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/venecia.jpg',
+                img: './img/pizzas/venecia.jpg',
                 precio: 900
             },
             {
                 nombre: "Milán",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/milan.jpg',
+                img: './img/pizzas/milan.jpg',
                 precio: 950
             },
             {
                 nombre: "Florencia",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/florencia.jpg',
+                img: './img/pizzas/florencia.jpg',
                 precio: 950
             },
             {
                 nombre: "Turín",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/turin.jpg',
+                img: './img/pizzas/turin.jpg',
                 precio: 1000
             },
             {
                 nombre: "Verona",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/verona.jpg',
+                img: './img/pizzas/verona.jpg',
                 precio: 1000
             },
             {
                 nombre: "Génova",
                 comentario: "Comentario 1",
-                img: './assets/pizzas/genova.jpg',
+                img: './img/pizzas/genova.jpg',
                 precio: 1000
             }
         ]
@@ -113,49 +113,49 @@ const ejemplo = [
             {
                 nombre: "Simple",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/simple.jpg',
+                img: './img/hamburguesas/simple.jpg',
                 precio: 800
             },
             {
                 nombre: "Nera",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/nera.jpg',
+                img: './img/hamburguesas/nera.jpg',
                 precio: 900
             },
             {
                 nombre: "Pollo",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/pollo.jpg',
+                img: './img/hamburguesas/pollo.jpg',
                 precio: 900
             },
             {
                 nombre: "Paltosa",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/paltosa.jpg',
+                img: './img/hamburguesas/paltosa.jpg',
                 precio: 950
             },
             {
                 nombre: "Cebollita",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/cebollita.jpg',
+                img: './img/hamburguesas/cebollita.jpg',
                 precio: 950
             },
             {
                 nombre: "Completa",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/completa.jpg',
+                img: './img/hamburguesas/completa.jpg',
                 precio: 1000
             },
             {
                 nombre: "Pepino",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/pepino.jpg',
+                img: './img/hamburguesas/pepino.jpg',
                 precio: 1000
             },
             {
                 nombre: "Asesina",
                 comentario: "Comentario 1",
-                img: './assets/hamburguesas/asesina.jpg',
+                img: './img/hamburguesas/asesina.jpg',
                 precio: 1000
             }
         ]
