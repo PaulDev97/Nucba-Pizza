04faa44ff493cd5ba2ef755583d8287791cd18b1
Your Name <perezfernando95@gmail.com>
2022-11-27 13:48:35 -0300

    JS updates

---
+++ objectCategory.js
@@ -0,0 +1,378 @@
+export const category = [
+
+  {
+    seccion: "populares",
+    lista: [
+      {
+        nombre: "Asesina",
+        comentario: "Comentario 1",
+        img: './img/populares/asesina.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Completa",
+        comentario: "Comentario 1",
+        img: './img/populares/completa.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Turín",
+        comentario: "Comentario 1",
+        img: './img/populares/turin.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Verona",
+        comentario: "Comentario 1",
+        img: './img/populares/verona.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Popular 5",
+        comentario: "Comentario 1",
+        img: './img/populares/wrap-carneEnCubos.jpeg',
+        precio: 800
+      },
+      {
+        nombre: "Popular 6",
+        comentario: "Comentario 1",
+        img: './img/populares/wrap-cerdo.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Popular 7",
+        comentario: "Comentario 1",
+        img: './img/populares/mexicanFood-dorados.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Popular 8",
+        comentario: "Comentario 1",
+        img: './img/populares/dulce-de-leche.jpg',
+        precio: 800
+      }
+    ]
+  },
+  {
+    seccion: "pizzas",
+    lista: [
+      {
+        nombre: "Roma",
+        comentario: "Comentario 1",
+        img: './img/pizzas/roma.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Nápoles",
+        comentario: "Comentario 1",
+        img: './img/pizzas/napoles.jpg',
+        precio: 900
+      },
+      {
+        nombre: "Venecia",
+        comentario: "Comentario 1",
+        img: './img/pizzas/venecia.jpg',
+        precio: 900
+      },
+      {
+        nombre: "Milán",
+        comentario: "Comentario 1",
+        img: './img/pizzas/milan.jpg',
+        precio: 950
+      },
+      {
+        nombre: "Florencia",
+        comentario: "Comentario 1",
+        img: './img/pizzas/florencia.jpg',
+        precio: 950
+      },
+      {
+        nombre: "Turín",
+        comentario: "Comentario 1",
+        img: './img/pizzas/turin.jpg',
+        precio: 1000
+      },
+      {
+        nombre: "Verona",
+        comentario: "Comentario 1",
+        img: './img/pizzas/verona.jpg',
+        precio: 1000
+      },
+      {
+        nombre: "Génova",
+        comentario: "Comentario 1",
+        img: './img/pizzas/genova.jpg',
+        precio: 1000
+      }
+    ]
+  },
+  {
+    seccion: "hamburguesas",
+    lista: [
+      {
+        nombre: "Simple",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/simple.jpg',
+        precio: 800
+      },
+      {
+        nombre: "Nera",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/nera.jpg',
+        precio: 900
+      },
+      {
+        nombre: "Pollo",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/pollo.jpg',
+        precio: 900
+      },
+      {
+        nombre: "Paltosa",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/paltosa.jpg',
+        precio: 950
+      },
+      {
+        nombre: "Cebollita",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/cebollita.jpg',
+        precio: 950
+      },
+      {
+        nombre: "Completa",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/completa.jpg',
+        precio: 1000
+      },
+      {
+        nombre: "Pepino",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/pepino.jpg',
+        precio: 1000
+      },
+      {
+        nombre: "Asesina",
+        comentario: "Comentario 1",
+        img: './img/hamburguesas/asesina.jpg',
+        precio: 1000
+      }
+    ]
+  },
+
+  {
+    seccion: "papas fritas",
+    lista: [
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "",
+        comentario: "",
+        img: "img/.jpg",
+        precio: 80,
+      },
+    ],
+  },
+
+  {
+    seccion: "wraps",
+    lista: [
+      {
+        nombre: "Vegetariano",
+        comentario: "Muy saludable",
+        img: "./assets/img/wraps-imgs/wrap-vegetariano.jpg",
+        precio: 120,
+      },
+      {
+        nombre: "Mediterraneo",
+        comentario: "Fresco y europeo",
+        img: "./assets/img/wraps-imgs/wrap-mediterraneo.jpg",
+        precio: 150,
+      },
+      {
+        nombre: "Mexicano con guacamole",
+        comentario: "Extremadamente picante",
+        img: "./assets/img/wraps-imgs/wrap-picante.jpeg",
+        precio: 90,
+      },
+      {
+        nombre: "Pollo al curry",
+        comentario: "Muy especiado",
+        img: "./assets/img/wraps-imgs/wrap-polloAlCurry.jpg",
+        precio: 100,
+      },
+      {
+        nombre: "Carne desmechada",
+        comentario: "Se te deshace en la boca",
+        img: "./assets/img/wraps-imgs/wrap-carneDesmechada.jpg",
+        precio: 170,
+      },
+      {
+        nombre: "Huevo y queso",
+        comentario: "Recomendable para la merienda",
+        img: "./assets/img/wraps-imgs/wrap-huevoYQueso.jpg",
+        precio: 110,
+      },
+      {
+        nombre: "Carne en cubos",
+        comentario: "El clásico",
+        img: "./assets/img/wraps-imgs/wrap-carneEnCubos.jpeg",
+        precio: 140,
+      },
+      {
+        nombre: "Carne de cerdo y verduras",
+        comentario: "Algo distinto",
+        img: "./assets/img/wraps-imgs/wrap-cerdo.jpg",
+        precio: 130,
+      },
+    ],
+  },
+
+  {
+    seccion: "mexican food",
+    lista: [
+      {
+        nombre: "Tacos al pastor",
+        comentario: "Carne marinada y suave",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-alPastor.jpg",
+        precio: 60,
+      },
+      {
+        nombre: "Tacos de Barbacoa",
+        comentario: "Carne desmechada y sabrosa",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-barbacoa.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "Tacos de suadero",
+        comentario: "Carne con mas grasa y sabor",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-suadero.jpg",
+        precio: 75,
+      },
+      {
+        nombre: "Tacos de pescado",
+        comentario: "Pescado rebozado y frito",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-pescado.jpg",
+        precio: 120,
+      },
+      {
+        nombre: "Tacos de birria",
+        comentario: "Tacos fritos para dipear",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-birria.jpg",
+        precio: 40,
+      },
+      {
+        nombre: "Tacos de cabeza",
+        comentario: "Carne con particular sabor",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-cabeza.jpg",
+        precio: 85,
+      },
+      {
+        nombre: "Tacos veganos",
+        comentario: "Para todos y todas",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-veganos.jpg",
+        precio: 95,
+      },
+      {
+        nombre: "Tacos dorados de pollo",
+        comentario: "Tortilla frita y adornados con crema",
+        img: "./assets/img/mexicanFood-imgs/mexicanFood-dorados.jpg",
+        precio: 160,
+      },
+    ],
+  },
+
+  {
+    seccion: "batidos",
+    lista: [
+      {
+        nombre: "Fresa y platano",
+        comentario: "Combinacion dulce",
+        img: "./assets/img/fresa-platano.jpg",
+        precio: 300,
+      },
+      {
+        nombre: "Tres frutos rojos",
+        comentario: "Aguante el rojo",
+        img: "./assets/img/frutos-rojos.jpg",
+        precio: 80,
+      },
+      {
+        nombre: "Batido Tropical",
+        comentario: "Suave",
+        img: "./assets/img/tropical.jpg",
+        precio: 150,
+      },
+      {
+        nombre: "Batido de mango",
+        comentario: "Welinton campeón",
+        img: "./assets/img/mango.jpg",
+        precio: 400,
+      },
+      {
+        nombre: "Chocolate con helado",
+        comentario: "Clasico",
+        img: "./assets/img/chocolate-helado.jpg",
+        precio: 250,
+      },
+      {
+        nombre: "Batido de nutella",
+        comentario: "Amante del sabor",
+        img: "./assets/img/nutella.jpg",
+        precio: 100,
+      },
+      {
+        nombre: "Chocolate vegano",
+        comentario: "Sabor unico",
+        img: "./assets/img/vegano.jpg",
+        precio: 60,
+      },
+      {
+        nombre: "Batido dulce de leche",
+        comentario: "Orgullo argentino",
+        img: "./assets/img/dulce-de-leche.jpg",
+        precio: 80,
+      },
+    ],
+  },
+];
