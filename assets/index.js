d37181eb3f0155a0a0943825a1596ce951b5508b
Leandro Federico Torales <leandrotorales1234@gmail.com>
2022-11-28 00:30:37 -0300

    se hizo logica de cards del carrito, falta lógica botones de carrito

--- assets/index.js
+++ assets/index.js
@@ -13,8 +13,12 @@ const categoryContainers = document.querySelectorAll(".category");
 const categoryTitle = document.querySelector(".container__categoty--title");
 const btnCart = document.querySelector(".btn-cart");
 const btnCartButtonClose = document.querySelector(".btn__cart--close");
+const btnComprarCart = document.querySelector(".btn__comprar--cart");
 const containerCartToggle = document.querySelector(".container__cart--toggle");
-const closeCart = document.querySelector(".close__cart");
+const containerCartCards = document.querySelector(".container__products--cart");
+/* const containerCart = document.querySelector(".container__cart"); */
+const totalCart = document.querySelector(".cart__total");
+const bubbleCountCart = document.querySelector(".cart__bubble--count");
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 const saveToLocalStorage = (key) => {
@@ -70,16 +74,15 @@ const renderCard = (lista) => {
   const { nombre, img, comentario, precio } = lista;
   return `
   <div class="popu-card">
-  <div class="popu-img-container" style="background-image: url(${img})" alt="${nombre}">
-  </div>
-  <h3 class="popu-name">${nombre}</h3>
-  <h3 class="popu-eslogan">${comentario}</h3>
-  <div class="popu-precio-btn-container">
-  <h2 class="popu-precio">$${precio}</h2>
-  <button class="popu-btn" data-name="${nombre}" data-precio="${precio}" data-comentario="${comentario}" data-img="${img}" >
-  Agregar
-  </button>
-  </div>
+    <div class="popu-img-container" style="background-image: url(${img})" alt="${nombre}"></div>
+    <h3 class="popu-name">${nombre}</h3>
+    <h3 class="popu-eslogan">${comentario}</h3>
+    <div class="popu-precio-btn-container">
+      <h2 class="popu-precio">$${precio}</h2>
+      <button class="popu-btn" data-nombre="${nombre}" data-precio="${precio}" data-comentario="${comentario}" data-img="${img}">
+        Agregar
+      </button>
+    </div>
   </div>
   `;
 };
@@ -110,9 +113,132 @@ const getCategoryTitle = () => {
 };
 console.log("getCategoryTitle:", getCategoryTitle());
 
-/* --------funciones para el carrito--------- */
+/* ------------------Logica del carrito------------------ */
+/* Funcion para ocultar y mostrar el menu */
 const toggleMenu = () => {
   containerCartToggle.classList.toggle("container__cart--toggle");
+};
+
+/* Render card cart */
+const renderProductCart = (product) => {
+  const { nombre, img, comentario, precio, cantidad } = product;
+  return `  <div class="render__products--cart">
+              <div class="image__container--cart" style="background-image: url(${img})"></div>
+              <div class="info__card--cart">
+                <h2 class="title__card--cart">${nombre}</h2>
+                <span class="comentario__card--cart">${comentario}</span>
+                <span class="price__card--cart">$${precio}</span>
+              </div>
+              <div class="amountSettings__card--cart">
+                <div class="container__minusButton--cart">
+                  <button class="button__minus--cart" data-nombre=${nombre}>-</button>
+                </div>
+                <span class="amount__card">${cantidad}</span>
+                <div class="container__addButton--cart">
+                  <button class="button__add--cart" data-nombre=${nombre}>+</button>
+                </div>
+              </div>
+            </div>
+          `;
+};
+
+/* Funcion renderizadora carrito card */
+const renderCart = () => {
+  if (!cart.length) {
+    containerCartCards.classList.add("correction");
+    containerCartCards.innerHTML = `<p class="cart__empty--message">No tienes productos en el carro.</p>`;
+    return;
+  } else {
+    containerCartCards.classList.remove("correction");
+    containerCartCards.innerHTML = cart.map(renderProductCart).join("");
+  }
+};
+
+/* Funcion obtener total precio cart */
+const getCartTotal = () => {
+  return cart.reduce((acc, cur) => acc + Number(cur.precio) * cur.cantidad, 0);
+};
+
+/* Funcion que rendeirza el total de la compra */
+const showTotal = () => {
+  totalCart.innerHTML = `$${getCartTotal().toFixed(2)}`;
+};
+
+/* Funcion obtener total de productos en carrito */
+const getTotalProducts = () => {
+  return cart.reduce((acc, cur) => acc + cur.cantidad, 0);
+};
+
+/* Funcion renderizadora para burbuja con el total de productos y ocultar o mostrar la bubble si hay o no hay productos en carrito*/
+const showBubbleCount = () => {
+  const numberCount = (bubbleCountCart.textContent = getTotalProducts());
+  if (numberCount !== 0) {
+    bubbleCountCart.classList.remove("disabled");
+  } else {
+    bubbleCountCart.classList.add("disabled");
+  }
+  console.log("cantidad de productos en carrito(bubble) ===>>", numberCount);
+  return numberCount;
+};
+
+/* Desabilitar boton de compra si no hay productos en el carrito */
+const btnDisable = (btn) => {
+  if (!cart.length) {
+    btn.classList.add("disabled");
+  } else {
+    btn.classList.remove("disabled");
+  }
+};
+
+/* Funcion que activa la mayoria de funciones, para renderizar card, guardar en LS, mostrar el total, desabilitar y habilitar botones */
+const checkStateCart = () => {
+  saveToLocalStorage(cart);
+  renderCart(cart);
+  showTotal();
+  btnDisable(btnComprarCart);
+  showBubbleCount(cart);
+};
+
+/* Funcion agregar producto al carrito */
+const addProductToCart = (e) => {
+  if (!e.target.classList.contains("popu-btn")) return;
+  const { nombre, precio, comentario, img } = e.target.dataset;
+
+  const setDataProduct = (nombre, precio, comentario, img) => {
+    return { nombre, precio, comentario, img };
+  };
+
+  const product = setDataProduct(nombre, precio, comentario, img);
+
+  const existProduct = (element) => {
+    console.log(element);
+    return cart.find((obj) => obj.nombre == element.nombre);
+  };
+
+  if (existProduct(product)) {
+    addUnitProduct(product);
+  } else {
+    createCartProduct(product);
+  }
+  /* Una vez que se termina de chequear si exister o sino existe, se ejecuta la funcion general que llama a las funciones renderizadoras y demás */
+  checkStateCart();
+};
+
+/* Funcion para crear la card de producto en el carrito */
+const createCartProduct = (product) => {
+  cart = [...cart, { ...product, cantidad: 1 }];
+};
+
+/* Agregar una unidad en el carrito si existe el producto */
+const addUnitProduct = (product) => {
+  cart = cart.map((cartProduct) =>
+    cartProduct.nombre == product.nombre
+      ? {
+          ...cartProduct,
+          cantidad: cartProduct.cantidad + 1,
+        }
+      : cartProduct
+  );
 };
 
 const init = () => {
@@ -123,9 +249,11 @@ const init = () => {
   containerCategories.addEventListener("click", getCategoryTitle);
   btnCart.addEventListener("click", toggleMenu);
   btnCartButtonClose.addEventListener("click", toggleMenu);
+  window.addEventListener("DOMContentLoaded", renderCart);
+  window.addEventListener("DOMContentLoaded", showTotal);
+  window.addEventListener("DOMContentLoaded", showBubbleCount);
+  window.addEventListener("DOMContentLoaded", btnDisable(btnComprarCart));
+  cardsContainer.addEventListener("click", addProductToCart);
 };
 
 init();
-
-/* Cambiar parametro por defecto a populares */
-/* Unir css de renders */
